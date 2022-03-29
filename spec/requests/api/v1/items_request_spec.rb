486e97d3beb2660a665e68f5e7aa94549647eb7f
Conor Barthel <conor.barthel@gmail.com>
2022-03-28 18:25:55 -0700

    Test: index and show page testing for item

---
+++ spec/requests/api/v1/items_request_spec.rb
@@ -0,0 +1,54 @@
+require 'rails_helper'
+
+describe "Items API" do
+  it "sends a list of items" do
+    create_list(:item, 3)
+
+    get '/api/v1/items'
+
+    expect(response).to be_successful
+
+    items = JSON.parse(response.body, symbolize_names: true)
+
+    expect(items.count).to eq(3)
+    item = items.first
+
+    expect(item).to have_key(:id)
+    expect(item[:id]).to be_an(Integer)
+
+    expect(item).to have_key(:name)
+    expect(item[:name]).to be_a(String)
+
+    expect(item).to have_key(:description)
+    expect(item[:description]).to be_a(String)
+
+    expect(item).to have_key(:unit_price)
+    expect(item[:unit_price]).to be_an(Float)
+
+    expect(item).to have_key(:merchant_id)
+    expect(item[:merchant_id]).to be_an(Integer)
+  end
+
+  it "can get one item by its id" do
+    id = create(:item).id
+
+    get "/api/v1/items/#{id}"
+
+    item = JSON.parse(response.body, symbolize_names: true)
+
+    expect(item).to have_key(:id)
+    expect(item[:id]).to be_an(Integer)
+
+    expect(item).to have_key(:name)
+    expect(item[:name]).to be_a(String)
+
+    expect(item).to have_key(:description)
+    expect(item[:description]).to be_a(String)
+
+    expect(item).to have_key(:unit_price)
+    expect(item[:unit_price]).to be_an(Float)
+
+    expect(item).to have_key(:merchant_id)
+    expect(item[:merchant_id]).to be_an(Integer)
+  end
+end
