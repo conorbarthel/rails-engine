1dd1868e3b2555486425d9768e7112e0fe3d8f81
Conor Barthel <conor.barthel@gmail.com>
2022-03-28 18:15:33 -0700

    fix: deleted new merchant test

--- spec/requests/api/v1/merchants_request_spec.rb
+++ spec/requests/api/v1/merchants_request_spec.rb
@@ -35,18 +35,4 @@ describe "Merchants API" do
     expect(merchant).to have_key(:name)
     expect(merchant[:name]).to be_a(String)
   end
-
-  it "can create a new merchant" do
-    merchant_params = ({
-                    name: 'Murder on the Orient Express',
-                  })
-    headers = {"CONTENT_TYPE" => "application/json"}
-
-
-    post "/api/v1/merchants", headers: headers, params: JSON.generate(merchant: merchant_params)
-    created_merchant = Book.last
-
-    expect(response).to be_successful
-    expect(created_merchant.name).to eq(merchant_params[:name])
-  end
 end
