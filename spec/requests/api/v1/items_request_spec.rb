af582732ff1191a815987613e6be28f5d1aa22cd
Conor Barthel <conor.barthel@gmail.com>
2022-03-30 13:59:27 -0700

    test: testing for new item request

--- spec/requests/api/v1/items_request_spec.rb
+++ spec/requests/api/v1/items_request_spec.rb
@@ -59,4 +59,24 @@ describe "Items API" do
     expect(item[:attributes]).to have_key(:merchant_id)
     expect(item[:attributes][:merchant_id]).to be_an(Integer)
   end
+
+  it "can create a new item" do
+    item_params = ({
+                    name: 'Photo Album',
+                    description: 'photos',
+                    unit_price: 999,
+                  })
+    headers = {"CONTENT_TYPE" => "application/json"}
+
+    post "/api/v1/items", headers: headers, params: JSON.generate(item: item_params)
+    created_item = Item.last
+
+    expect(response).to be_successful
+    expect(created_item.name).to eq(item_params[:name])
+    expect(created_item.description).to eq(item_params[:description])
+    expect(created_item.unit_price).to eq(item_params[:unit_price])
+    expect(created_item.merchant_id).to eq(item_params[:merchant_id])
+  end
+
+  
 end
