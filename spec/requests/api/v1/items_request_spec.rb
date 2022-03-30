f1e33f22075a04b3c6cba27a2273f541ea821df4
Conor Barthel <conor.barthel@gmail.com>
2022-03-30 11:58:19 -0700

    Feat: item show request

--- spec/requests/api/v1/items_request_spec.rb
+++ spec/requests/api/v1/items_request_spec.rb
@@ -2,54 +2,61 @@ require 'rails_helper'
 
 describe "Items API" do
   it "sends a list of items" do
-    create_list(:item, 3)
+    merch = create(:merchant)
+    mo_merch = create(:merchant)
+    items = create_list(:item, 5, merchant: merch)
+    mo_items = create_list(:item, 5, merchant: mo_merch)
 
-    get '/api/v1/items'
+    get api_v1_items_path
 
     expect(response).to be_successful
 
     items = JSON.parse(response.body, symbolize_names: true)
 
-    expect(items.count).to eq(3)
-    item = items.first
+    expect(items[:data].count).to eq(10)
+    item = items[:data].first
 
     expect(item).to have_key(:id)
-    expect(item[:id]).to be_an(Integer)
+    expect(item[:id]).to be_an(String)
 
-    expect(item).to have_key(:name)
-    expect(item[:name]).to be_a(String)
+    expect(item[:attributes]).to have_key(:name)
+    expect(item[:attributes][:name]).to be_a(String)
 
-    expect(item).to have_key(:description)
-    expect(item[:description]).to be_a(String)
+    expect(item[:attributes]).to have_key(:description)
+    expect(item[:attributes][:description]).to be_a(String)
 
-    expect(item).to have_key(:unit_price)
-    expect(item[:unit_price]).to be_an(Float)
+    expect(item[:attributes]).to have_key(:unit_price)
+    expect(item[:attributes][:unit_price]).to be_an(Float)
 
-    expect(item).to have_key(:merchant_id)
-    expect(item[:merchant_id]).to be_an(Integer)
+    expect(item[:attributes]).to have_key(:merchant_id)
+    expect(item[:attributes][:merchant_id]).to be_an(Integer)
   end
 
   it "can get one item by its id" do
-    merchant = Merchant.create!(name: 'Coca-Cola')
-    id = create(:item).id
+    merch = create(:merchant)
+    mo_merch = create(:merchant)
+    items = create_list(:item, 5, merchant: merch)
+    mo_items = create_list(:item, 5, merchant: mo_merch)
+    item = items.first
 
-    get "/api/v1/items/#{id}"
+    get api_v1_item_path(item)
 
     item = JSON.parse(response.body, symbolize_names: true)
+    item = item[:data]
 
     expect(item).to have_key(:id)
-    expect(item[:id]).to be_an(Integer)
+    expect(item[:id]).to be_an(String)
 
-    expect(item).to have_key(:name)
-    expect(item[:name]).to be_a(String)
+    expect(item[:attributes]).to have_key(:name)
+    expect(item[:attributes][:name]).to be_a(String)
 
-    expect(item).to have_key(:description)
-    expect(item[:description]).to be_a(String)
+    expect(item[:attributes]).to have_key(:description)
+    expect(item[:attributes][:description]).to be_a(String)
 
-    expect(item).to have_key(:unit_price)
-    expect(item[:unit_price]).to be_an(Float)
+    expect(item[:attributes]).to have_key(:unit_price)
+    expect(item[:attributes][:unit_price]).to be_an(Float)
 
-    expect(item).to have_key(:merchant_id)
-    expect(item[:merchant_id]).to be_an(Integer)
+    expect(item[:attributes]).to have_key(:merchant_id)
+    expect(item[:attributes][:merchant_id]).to be_an(Integer)
   end
 end
